a9ea0357343528486c8f8871257e9f6e720f07e0
misbahiradat <sassyknight99@gmail.com>
2023-10-31 19:50:56 +0500

    Updated_Code

---
+++ LeNET/src/splitfolder.py
@@ -0,0 +1,9 @@
+# Author @ Misbah Iradat
+# import libraries
+import splitfolders as sf
+
+# path
+FOLDER_NAME = '../../data/eye_data_main/'
+
+# splitfolder
+sf.ratio(FOLDER_NAME, output="../../data/aug_data_imb", seed=142, ratio=(0.7,0.25,0.05))
---
+++ custom_model/app/app.py
@@ -0,0 +1,23 @@
+# Author @ Misbah Iradat
+import uvicorn
+from fastapi import FastAPI
+import REDEye_detection as live_predict
+
+
+app = FastAPI()
+
+@app.get('/')
+async def project():
+    return 'Red Eye Detection'
+
+@app.post('/app/REDEye_detection')
+async def predict_call():
+
+    pred = live_predict
+    return pred
+   
+# uvicorn app:app --reload
+if __name__ =="__main__":
+
+    uvicorn.run(app,port =8000, host ='0.0.0.0')
+
